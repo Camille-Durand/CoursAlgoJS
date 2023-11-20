8e9514b4984bbacab6a7866cfae916eb984ce4a5
Camille-Durand <camillefanguin@gmail.com>
2023-11-20 16:11:01 +0100

    Code interface calcul choco + vérif si null

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-function chocolatine() {
-    choco = parseInt(prompt("Cmb de choco?"));
-    total = 0;
+function calculer() {
+    const quantity = document.getElementById("quantite");
+    const choco = quantity.value;
+
+    const result = document.querySelector('p');
+
+    if (choco == 0) {
+        result.textContent = "Veuillez choisir le nombre de chocolatine";
+    } else if (choco <0) {
+        result.textContent = "Veuillez rentrer une valeur positive";
+    } else {
+        total = 0;
         if (choco < 10){
             total = 1.4 * choco;
         } else if(choco <= 20) {
@@ -8,5 +17,6 @@ function chocolatine() {
         } else if (choco > 20) {
             total = 1.4 * 10 + (10)*1.3 + (choco -20)*1.2;
         }
-    console.log("Pour",choco,"choco, le prix est:",total.toFixed(2),"€");
+        result.textContent = "Le prix à payer est de " + total.toFixed(2) + "€";
+    }
 }
