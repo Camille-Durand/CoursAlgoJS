22f3df747544f26d3379a4c4483ac2840c8fd841
Camille-Durand <camillefanguin@gmail.com>
2023-11-20 16:28:43 +0100

    Code remplacement

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-function prixTTC() {
-    let result = '';
-
-    let getArticle = document.getElementById("article");
-    let nameArticle = getArticle.value;
-    let getPrice = document.getElementById("price");
-    let PriceArticle = getPrice.value;
-    let getQuantity = document.getElementById("quantity");
+function calculer() {
+    let getPrice = document.getElementById("prixHt");
+    let priceArticle = getPrice.value;
+    let getQuantity = document.getElementById("quantite");
     let quantityArticle = getQuantity.value;
 
-    let tab = [nameArticle,PriceArticle,quantityArticle];
-    console.log(tab);
+    let TTC = priceArticle*1.20*quantityArticle;
 
-    let TTC = PriceArticle*1.20*quantityArticle;
-    console.log(TTC);
-    document.body.innerHTML = document.body.innerHTML.replace("?", TTC);
+    const result = document.querySelector('p');
+    result.textContent = "Le prix TTC est de " + TTC.toFixed(2) + "€";
 }
