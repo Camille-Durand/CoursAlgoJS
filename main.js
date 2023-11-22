f549525745719c6253d5ffe8732c14bfbc4fff4d
Camille-Durand <camillefanguin@gmail.com>
2023-11-22 10:15:43 +0100

    Code localStorage & addTasks

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-function addTask() {
-    let getDiv = document.getElementById('tasks');
+//création du tableau
+localStorage.getItem('tasks') == null ? localStorage.setItem("tasks", []):false;
 
+function addTask() {
+    //le code ci-dessous  : récupére la clé tasks dans le localstorage 
+    //et le met à jour avec la valeur saisie dans l'input du formulaire
+    //récupération de l'input ('#task)
+    let valeur = document.querySelector('#task').value;
+    //récupérer dans une variable la valeur clé ('tasks')
+    let tasks = localStorage.getItem('tasks');
+    //test si la clé tasks dans localstorage est vide
+    if(tasks==""){
+      tasks+= valeur;
+    }
+    //test sinon elle n'est pas vide
+    else{
+        //transforme en tableau
+        tasks = tasks.split(",");
+        //ajoute la valeur de l'input au tableau
+        tasks.push(valeur);
+    }
+    //mise à jour de la clé
+    localStorage.setItem('tasks',tasks);
+
+    let getDiv = document.getElementById('tasks');
     const div = document.createElement('div');
     getDiv.appendChild(div);
     const para = document.createElement('p');
@@ -20,6 +42,33 @@ function addTask() {
     updateT.textContent = "Update la tâche";
     updateT.setAttribute("onclick", "updateTask(this)");
     div.appendChild(updateT);
+}
+
+function showAllTasks() {
+    let allTasks = localStorage.getItem('tasks').split(',');
+    console.log(allTasks);
+
+    for(let i = 0; i < allTasks.length; i++) {
+        let getDiv = document.getElementById('tasks');
+        const div = document.createElement('div');
+        getDiv.appendChild(div);
+
+        const para = document.createElement('p');
+        div.appendChild(para);
+        para.textContent = allTasks[i];
+
+        const deleteT = document.createElement('button');
+        deleteT.id = "delete";
+        deleteT.textContent = "Supprimer la tâche";
+        deleteT.setAttribute("onclick", "deleteTask(this)");
+        div.appendChild(deleteT);
+
+        const updateT = document.createElement('button');
+        updateT.id = "update";
+        updateT.textContent = "Update la tâche";
+        updateT.setAttribute("onclick", "updateTask(this)");
+        div.appendChild(updateT);
+    }
 }
 
 function deleteTask(e) {
@@ -54,7 +103,6 @@ function updateTask(e) {
         statut = true;
     }
 }
-
 
 function reload() {
     window.location.reload();
