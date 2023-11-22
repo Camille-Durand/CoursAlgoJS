db48c1551c8bd3b465145ecf884ef4125d57e7c5
Camille-Durand <camillefanguin@gmail.com>
2023-11-22 16:32:39 +0100

    Code en cours

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let url = "https://adresse.data.gouv.fr/api-doc/adresse";
-let api = fetch(url)
-            .then(async response=> {
-                console.log(response);
-                const json = await response.json();
-                console.log(json);
-            });
+let adresse = document.getElementById('ville_d').value;
+document.getElementById('bt').addEventListener("click", distance);
+function distance() {
+    let url = "https://api-adresse.data.gouv.fr/search/?q=Toulouse";
+    let api = fetch(url)
+                .then(async response=> {
+                    console.log(response);
+                    const json = await response.json();
+                    console.log(json);
+
+                    console.log(json.features[0].properties.label);
+                    console.log(json.features[0].coordinates);
+                    console.log(json.features[0].coordinates);
+                });
+}
